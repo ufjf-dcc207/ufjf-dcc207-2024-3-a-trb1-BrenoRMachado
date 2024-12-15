4a6c075325f725472b25b94e9eb9e775021b1222
Breno Machado <burracha.6464@gmail.com>
2024-12-15 14:52:02 -0300

    Arranja os dados dos alimentos para um arquivo independente e externo

---
+++ src/Alimentos.ts
@@ -0,0 +1,18 @@
+import {ham1, ham2, ham3, ham4, ham5, ham6, ham7, ham8, ham9, ham10} from "./assets/index.ts"
+
+type AlimentosTupleType = [string, string, string, number];
+
+const ALIMENTOS: AlimentosTupleType[] = [
+    [ham1,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+    [ham2,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+    [ham3,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+    [ham4,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+    [ham5,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+    [ham6,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+    [ham7,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+    [ham8,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+    [ham9,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+    [ham10,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+];
+
+export default ALIMENTOS;
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
 import './App.css'
 import Alimentos from './Componentes/Alimentos/Alimentos'
 import { ReactNode } from "react";
-import {ham1, ham2, ham3, ham4, ham5, ham6, ham7, ham8, ham9, ham10} from "./assets/index.ts"
-
-type AlimentosTupleType = [string, string, string, number];
-
-const ALIMENTOS: AlimentosTupleType[] = [
-    [ham1,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
-    [ham2,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
-    [ham3,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
-    [ham4,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
-    [ham5,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
-    [ham6,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
-    [ham7,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
-    [ham8,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
-    [ham9,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
-    [ham10,"Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
-];
+import ALIMENTOS from './Alimentos';
 
 function App() {
   const comidas: ReactNode[] = ALIMENTOS.map((alimento) => (
