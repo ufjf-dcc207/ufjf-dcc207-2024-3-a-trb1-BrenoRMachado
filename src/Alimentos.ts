8450a8b257f6cdac3a9402e6ea0f2869b9d58bdc
Caíque Guedes <caiqueluizghpo@gmail.com>
2024-12-15 15:15:52 -0300

    melhora o x-gato

--- src/Alimentos.ts
+++ src/Alimentos.ts
@@ -9,7 +9,7 @@ const ALIMENTOS: AlimentosTupleType[] = [
     [ham4,"SweetBurguer","Pão, Nutella, pedaços de morango e açúcar.",35.50],
     [ham5,"Sushiburguer","Arroz prensado, salmão,alga, cream cheese e wasabi", 45.50],
     [ham6,"V-Gano","Pão de alface, bife de soja, rúcula, cebola roxa e picles.",32.00],
-    [ham7,"X-gato","Pão e bife felinos, queijo, picles e muito miau.",25.90],
+    [ham7,"X-gato","Pão felino, bife felino, queijo, picles e muito miau.",25.90],
     [ham8,"Hamburguer de Siri","Pão, ingrediente secreto, alface, tomate, cebola e queijo.",42.00],
     [ham9,"Cheespider","Hamburguer vivo feito de pão, bife, salada e molhos",540.00],
     [ham10,"Bulburguer","Bulbassauro burguer vivo com o X-burguer em suas costas.",95.50],
