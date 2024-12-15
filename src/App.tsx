04b09c67a01f0e2d3b349a9d31953f22c36b9bdd
Caíque Guedes <caiqueluizghpo@gmail.com>
2024-12-15 13:33:37 -0300

    cria uma tupla e percorre a tupla usando o map

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css'
+import Alimentos from './Componentes/Alimentos/Alimentos'
+import { ReactNode } from "react";
+
+type AlimentosTupleType = [string, string, number];
+
+const ALIMENTOS: AlimentosTupleType[] = [
+    ["Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+    ["Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+    ["Duble Onion Cheddar","Pão, dupla carne bovina, aneis de cebola, cheddar e bacon.",23.00],
+];
 
 function App() {
+  const comidas: ReactNode[] = ALIMENTOS.map((alimento) => (
+    <Alimentos
+      nome={alimento[0]}
+      descricao={alimento[1]}
+      preco={alimento[2]} 
+    />  
+));
   return (
     <div>
-      
+      {comidas}
     </div>
   )
 }
